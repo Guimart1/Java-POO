4348a9947e1174dbde15fef25a1d7a31f8ade1fa
Admin <Admin@L1GNZ06>
2023-09-20 16:53:51 -0300

    Atualização Calculadora

--- prjCalculadora/src/prjCalculadora/App.java
+++ prjCalculadora/src/prjCalculadora/App.java
@@ -7,10 +7,6 @@ public class App {
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 
-		
-
-
-
         Object[] opcoes = {"Adição", "Subtração", "Multiplicação", "Divisão"};
         Object selecaoOpcao = JOptionPane.showInputDialog(null, "Escolha a operação:", "Menu", JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]);
         String opcao = selecaoOpcao.toString();
@@ -21,37 +17,38 @@ public class App {
         String opcaoTipo = selecaoTipo.toString();
         
         String numStr = JOptionPane.showInputDialog("Digite o primeiro número: ");
-		calc.num1 = Double.parseDouble(numStr);
+        calc.setNum1 (Double.parseDouble(numStr));
+		
 		numStr = JOptionPane.showInputDialog("Digite o segundo número: ");
-		calc.num2 = Double.parseDouble(numStr);
+		calc.setNum2 (Double.parseDouble(numStr));
 		
         switch(opcao) {
         	case "Adição":
         		if (opcaoTipo == "Inteiro") {
             		calc.Soma();
         		}else {
-        			JOptionPane.showMessageDialog(null, calc.Soma(calc.num1, calc.num2));
+        			JOptionPane.showMessageDialog(null, calc.Soma(calc.getNum1(),calc.getNum2()));
         		}
         		break;
         	case "Subtração":
         		if (opcaoTipo == "Inteiro") {
             		calc.Subtracao();
         		}else {
-        			JOptionPane.showMessageDialog(null, calc.Subtracao(calc.num1, calc.num2));
+        			JOptionPane.showMessageDialog(null, calc.Subtracao(calc.getNum1(), calc.getNum2()));
         		}
         		break;
         	case "Multiplicação":
         		if (opcaoTipo == "Inteiro") {
             		calc.Multiplicacao();
         		}else {
-        			JOptionPane.showMessageDialog(null, calc.Multiplicacao(calc.num1, calc.num2));
+        			JOptionPane.showMessageDialog(null, calc.Multiplicacao(calc.getNum1(), calc.getNum2()));
         		}
         		break;
         	case "Divisão":
         		if (opcaoTipo == "Inteiro") {
             		calc.Divisao();
         		}else {
-        			JOptionPane.showMessageDialog(null, calc.Divisao(calc.num1, calc.num2));
+        			JOptionPane.showMessageDialog(null, calc.Divisao(calc.getNum1(), calc.getNum2()));
         		}
         		break;
         }
@@ -59,3 +56,4 @@ public class App {
 	}
 
 }
+
--- prjCalculadora/src/prjCalculadora/Calculadora.java
+++ prjCalculadora/src/prjCalculadora/Calculadora.java
@@ -2,68 +2,98 @@ package prjCalculadora;
 import javax.swing.JOptionPane;
 import java.text.DecimalFormat;
 public class Calculadora {
+	//Limitar caracteres na variavel double
 	DecimalFormat decimal = new DecimalFormat("0.0000");
-	public double num1;
-	public double num2;
-	public double r;
-	public String resp;
+	//Atributos de cálculo
+	private double num1;
+	private double num2;
+	private double result;
+	private String respostaDouble;
 	
-
-
+	//Getters e setters dos atributos principais
+	public double getNum1() {
+		return num1;
+	}
+	public void setNum1(double num1) {
+		this.num1 = num1;
+	}
+	public double getNum2() {
+		return num2;
+	}
+	public void setNum2(double num2) {
+		this.num2 = num2;
+	}
+	
+	
+	//Método de soma de números inteiros
 	public void Soma() {
+		//Conversão de número int para double
 		int numInt1 = (int)num1;
 		int numInt2 = (int)num2;
-		int r = (int)numInt1+numInt2;
-		JOptionPane.showInternalMessageDialog(null, "O resultado da soma é: "+r);
+		int respNum = (int)numInt1+numInt2;
+		JOptionPane.showInternalMessageDialog(null, "O resultado da soma é: "+respNum);
 	}
+	//Método de soma de números decimais
 	public String Soma(double num1, double num2) {
-		r = num1+num2;
-		resp = "O resultado da soma é: "+decimal.format(r);
-		return resp;
+		result = num1+num2;
+		respostaDouble = "O resultado da soma é: "+decimal.format(result);
+		return respostaDouble;
 	}
 	
+	//Método de subtração de numeros inteiros
 	public void Subtracao() {
+		//Conversão de número int para double
 		int numInt1 = (int)num1;
 		int numInt2 = (int)num2;
-		r = (int)numInt1-numInt2;
-		JOptionPane.showInternalMessageDialog(null, "O resultado da subtração é: "+r);
-	}
-	public String Subtracao(double num1, double num2) {
-		r = num1-num2;
-		resp = "O resultado da soma é: "+decimal.format(r);
-		return resp;
+		int result = (int)numInt1-numInt2;
+		JOptionPane.showInternalMessageDialog(null, "O resultado da subtração é: "+result);
 	}
 	
+	//Método de subtração de numeros decimais
+	public String Subtracao(double num1, double num2) {
+		result = num1-num2;
+		respostaDouble = "O resultado da soma é: "+decimal.format(result);
+		return respostaDouble;
+	}
+	
+	//Método de divisão de numeros inteiros
 	public void Divisao() {
+		//Conversão de número int para double
 		int numInt1 = (int)num1;
 		int numInt2 = (int)num2;
 		if (numInt1 == 0 || numInt2 == 0) {
 			JOptionPane.showMessageDialog(null, "Não é possível fazer o cálculo divindo e(ou) sendo dividido por zero.");
 		}else {
-			r = (int)numInt1/numInt2;
-			JOptionPane.showInternalMessageDialog(null, "O resultado da divisão é: "+decimal.format(r));
+			result = (int)numInt1/numInt2;
+			JOptionPane.showInternalMessageDialog(null, "O resultado da divisão é: "+decimal.format(result));
 		}
 	}
+	
+	//Método de divisão de numeros decimais
 	public String Divisao(double num1, double num2) {
 		if (num1 == 0 || num2 == 0) {
-			return resp = "Não é possível fazer o cálculo divindo e(ou) sendo dividido por zero.";
+			return respostaDouble = "Não é possível fazer o cálculo divindo e(ou) sendo dividido por zero.";
 		}else {
-			r = num1/num2;
-			resp = "O resultado da soma é: "+decimal.format(r);
-			return resp;
+			result = num1/num2;
+			respostaDouble = "O resultado da soma é: "+decimal.format(result);
+			return respostaDouble;
 		}
 
 	}
 	
+	//Método de multiplicação de numeros inteiros
 	public void Multiplicacao() {
+		//Conversão de número int para double
 		int numInt1 = (int)num1;
 		int numInt2 = (int)num2;
-		r = (int)numInt1*numInt2;
+		int r = (int)numInt1*numInt2;
 		JOptionPane.showInternalMessageDialog(null, "O resultado da multiplicação é: "+r);
 	}
+	//Metodo de multiplicação de numeros decimaims
 	public String Multiplicacao(double num1,double num2) {
-		r = num1*num2;
-		resp = "O resultado da soma é: "+decimal.format(r);
-		return resp;
+		result = num1*num2;
+		respostaDouble = "O resultado da soma é: "+decimal.format(result);
+		return respostaDouble;
 	}
 }
+
